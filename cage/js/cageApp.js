0b20f3e31e558708b65ce2e0e3bf9d01f3a9be46
Mark Pooley <mawwwk@gmail.com>
2015-06-24 12:08:24 -0500

    update cage page

--- cage/js/cageApp.js
+++ cage/js/cageApp.js
@@ -1,12 +1,12 @@
 //have divs fade in and out based when scrolled to/away
-$(document).scroll(function(){
-	$('div').each(function(){
-		var y = $(document).scrollTop();
-		var t = $(this).parent().offset().top;
-		if (y > t) {
-			$(this).fadIn();
-		} else {
-			$this.fadeOut();
-		}
-	})
-})
+//$(document).scroll(function(){
+//	$('div').each(function(){
+//		var y = $(document).scrollTop();
+//		var t = $(this).parent().offset().top;
+//		if (y > t) {
+//			$(this).fadeIn();
+//		} else {
+//			$(this).fadeOut();
+//		}
+//	})
+//})//
